d33aa2f657380188fa6c9bba496bea304a1a3408
Nguyen Manh Linh <nmlinh2799@gmail.com>
2020-11-28 22:03:01 +0700

    Add mana bar

--- Assets/Scripts/Inventory.cs
+++ Assets/Scripts/Inventory.cs
@@ -1,10 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Inventory : MonoBehaviour
 {
     //References
+    [SerializeField] float maxMana = 10f;
+    [SerializeField] float manaConsumptionAmount = 3f;
+    [SerializeField] float manaRegenerationAmount = 1f;
+    [SerializeField] Image manaBarImg;
     [SerializeField] GameObject projectilePrefab;
     [SerializeField] List<GameObject> items;
     Transform cannon;
@@ -16,33 +21,44 @@ public class Inventory : MonoBehaviour
     [SerializeField] KeyCode activateItem3Key;
 
     public bool isDisabled = false;
+    float currentMana;
 
     // Start is called before the first frame update
     void Start()
     {
         cannon = GetComponent<Cannon>().cannon;
+        currentMana = maxMana;
     }
 
     // Update is called once per frame
     void Update()
     {
+        manaBarImg.fillAmount = currentMana / maxMana;
         if (GetComponent<Move>().isMyTurn)
         {
             if (Input.GetKeyDown(shootKey))
             {
                 Instantiate(projectilePrefab, cannon.position, cannon.rotation);
+                currentMana += manaRegenerationAmount;
+                if (currentMana > maxMana)
+                {
+                    currentMana = maxMana;
+                }
             }
-            else if (Input.GetKeyDown(activateItem1Key) && !isDisabled)
+            else if (Input.GetKeyDown(activateItem1Key) && !isDisabled && currentMana >= manaConsumptionAmount)
             {
                 Instantiate(items[0], cannon.position, cannon.rotation);
+                currentMana -= manaConsumptionAmount;
             }
-            else if (Input.GetKeyDown(activateItem2Key) && !isDisabled)
+            else if (Input.GetKeyDown(activateItem2Key) && !isDisabled && currentMana >= manaConsumptionAmount)
             {
                 Instantiate(items[1], cannon.position, cannon.rotation);
+                currentMana -= manaConsumptionAmount;
             }
-            else if (Input.GetKeyDown(activateItem3Key) && !isDisabled)
+            else if (Input.GetKeyDown(activateItem3Key) && !isDisabled && currentMana >= manaConsumptionAmount)
             {
                 Instantiate(items[2], cannon.position, cannon.rotation);
+                currentMana -= manaConsumptionAmount;
             }
         }
     }
