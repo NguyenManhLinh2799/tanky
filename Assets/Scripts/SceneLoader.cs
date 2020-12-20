486973828b38a074a5150edbfe593d7f59360f39
Nguyen Tan Hung <nthungnt2608@gmail.com>
2020-12-21 00:20:22 +0700

    add click sound

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -5,28 +5,41 @@ using UnityEngine.SceneManagement;
 
 public class SceneLoader : MonoBehaviour
 {
+    AudioSource audioSource;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
     public void LoadPlayer1ChoosingTank()
     {
+        audioSource.Play();
+
         SceneManager.LoadScene("Player 1 Choosing Tank");
     }
 
     public void LoadPlayer2ChoosingTank()
     {
+        audioSource.Play();
         SceneManager.LoadScene("Player 2 Choosing Tank");
     }
 
     public void LoadPlayer1ChoosingItems()
     {
+        audioSource.Play();
         SceneManager.LoadScene("Player 1 Choosing Items");
     }
 
     public void LoadPlayer2ChoosingItems()
     {
+        audioSource.Play();
         SceneManager.LoadScene("Player 2 Choosing Items");
     }
 
     public void LoadMainScene()
     {
+        audioSource.Play();
         SceneManager.LoadScene("Main Game");
     }
 }
